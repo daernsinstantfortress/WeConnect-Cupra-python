1906a099ebb7573496af3494080024468dbdada3
daern <daern@instantfortress.co.uk>
2026-05-21 06:28:05 +0000

    Fix for API change and bump version

--- weconnect_cupra/auth/auth_util.py
+++ weconnect_cupra/auth/auth_util.py
@@ -1,4 +1,8 @@
 def addBearerAuthHeader(token, headers=None):
     headers = headers or {}
     headers['Authorization'] = f'Bearer {token}'
+    headers["app-market"] = "android"
+    headers["app-brand"] = "cupra"
+    headers["app-version"] = "2.15.0"
+    headers["origin"] = "app"
     return headers
